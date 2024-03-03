bf4a8c30f27940a7d9533536f9a1cc0ededb4a08
ili101 <zoby101@gmail.com>
2024-03-03 23:58:08 +0000

    Clean

--- .devcontainer/install-dev-tools.sh
+++
@@ -1,8 +0,0 @@
-# Trust all repositories
-git config --global --add safe.directory '*'
-
-# Install PowerShell
-## DevContainer feature - not working with Alpine https://github.com/devcontainers/features/issues/890
-## From APK repository
-apk add --no-cache powershell
-## Alternatively get last version from archive https://learn.microsoft.com/en-us/powershell/scripting/install/install-alpine
